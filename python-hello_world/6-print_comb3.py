906e01c9d0664d7d8478199210b91f400704d11b
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-19 20:11:25 +0300

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,9 @@
+combinations = []
 for num1 in range(9):
     for num2 in range(num1 + 1, 10):
-        print("{:d}{:d}, ".format(num1, num2), end="")
+        combinations.append("{:d}{:d}".format(num1, num2))
+
+output = ", ".join(combinations)
+print(output)
+
 
