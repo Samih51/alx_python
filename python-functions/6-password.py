05397645307853403749cbf1dc2a23e85ec695d7
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-19 22:24:27 +0300

    Update 6-password.py

---
+++ python-functions/6-password.py
@@ -0,0 +1,21 @@
+def validate_password(password):
+    length = len(password)
+    check_uppercase=0
+    check_lowercase=0
+    if length<8:
+        return False
+    for num in range(length):
+        if password[num].isupper():
+            check_uppercase+=1
+        if password[num].islower():
+            check_lowercase+=1
+        if password[num] == " ":
+            return False
+    if check_uppercase == 0 or check_lowercase == 0:
+        return False
+    return True
+
+print(validate_password("Password123"))
+print(validate_password("abc123"))
+print(validate_password("Password 123"))
+print(validate_password("password123")) 
