7154c53571ae090742b5fffaee8eb84176611a32
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-19 20:14:28 +0300

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,9 +1,5 @@
-combinations = []
 for num1 in range(9):
     for num2 in range(num1 + 1, 10):
-        combinations.append("{:d}{:d}".format(num1, num2))
-
-output = ", ".join(combinations)
-print(output)
-
-
+        print("{:d}{:d}".format(num1, num2), end="")
+        if num1 != 8 or num2 != 9:
+            print(", ", end="")
