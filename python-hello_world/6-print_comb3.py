e980e99319539eeb053f2eee1404cdc06bd27e4a
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-19 20:01:07 +0300

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,4 @@
-for num1 in range(10):
+for num1 in range(9):
     for num2 in range(num1 + 1, 10):
-        print("{:02d}, ".format(num1), end="")
-        print("{:02d}, ".format(num2), end="")
+        print("{:d}{:d}, ".format(num1, num2), end="")
 
-print()
