3efd4ef319ef1313e87220288155922533a7a343
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-19 20:25:30 +0300

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,11 +4,10 @@ number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
 
-print("The string Last digit of", number, "is", last_digit, end=" ")
+output = "The string Last digit of, " + str(number) + ", is " + str(last_digit) + " and is"
 
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
-else:
-    print("and is less than 6 and not 0")
+output += " greater than 5" * (last_digit > 5)
+output += " 0" * (last_digit == 0)
+output += " less than 6 and not 0" * (last_digit < 6 and last_digit != 0)
+
+print(output)
