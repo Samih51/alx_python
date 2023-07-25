2ace7b026f4b2fcac0ffc7e2a1754ea500010e56
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-25 22:02:30 +0300

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,10 +4,14 @@ if __name__=="__main__":
     a=len(argv)
     if a==1:
         print("0 arguments.")
+    elif a==2:
+        print("1 argument:")
+        print ("1: {}".format(argv[1]))
+
     else:
+        print("{} arguments:".format(a-1))
         i=1
 
         while (i<a):
-           
             print ("{}: {}".format(i,argv[i]))
             i+=1
