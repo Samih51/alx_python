279169d6607401e611edc63452dc78cb607d8125
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-26 11:30:34 +0300

    Update 1-print_matrix_integer.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     while(i<outside_len):
         inside_len=len(matrix[i])
         while (j<inside_len):
-            if j==(len-1):
+            if j==(inside_len-1):
                 print("{:d}".format(matrix[i][j]),end ="")
             else:
                 print("{:d} ".format(matrix[i][j]),end ="")
