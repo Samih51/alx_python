d7f9dbd2c38a996bb9a2b5c72fc785e0601e79f4
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-26 10:10:20 +0300

    Update 3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -11,3 +11,4 @@ def safe_print_division(a,b):
         print("An unexpected error occurred:", e)
     finally:
         print("Inside result:{}".format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
