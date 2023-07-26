b04057141475ad7aef9eeef0397090bab9e6a454
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-26 10:46:47 +0300

    Update 0-no_c.py

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,8 +1,7 @@
 def no_c(my_string):
     length=len(my_string)
-    i=0
-    while(i<length):
-        if (my_string[i]=="c" | my_string[i]=="C"):
-            del my_string[i]
-        i+=1
-    return my_string
+    a= my_string.replace("c","")
+    b= my_string.replace("C","")
+    
+    return b
+print(no_c("chello"))
