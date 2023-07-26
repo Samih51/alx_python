0055efc72541f1629732736770e4b045fcdc2129
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-07-26 22:08:24 +0300

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,12 @@
 def square_matrix_simple(matrix=[]):
-    square=(map(lambda x : x**2,matrix))
+    i=0
+    length=len(matrix)
+    square=[]
+    while(i<length):
+        result = map(lambda x : x**2, matrix[i])
+        square.append(list(result))
+        i+=1
+
     print (square)
     print (matrix)
+square_matrix_simple([[1,2,3],[4,5]])
