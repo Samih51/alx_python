da5a8587eaf49d26179f6bb85877f0b4be8d102d
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-01 22:47:57 +0300

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -17,7 +17,7 @@ class Square:
         if self.__size < 0:
             raise ValueError("size must be >= 0")
         
-    @property
+    @size.setter
     def size(self):
         """Returns the value of size. It is a getter method"""
        
