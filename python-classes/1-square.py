8c9a0608992ffedc7e451a8f302eeb208399774f
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-01 21:48:39 +0300

    Create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+""" this class represents a square"""
+class Square:
+    """ 
+        defineing the size of a square for later on use
+            """
+    def __init__(self,size):
+        self.__size = size
+    
