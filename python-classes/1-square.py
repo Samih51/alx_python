17a407b7a942a16dd5a5ccbcb0e411f2d820d123
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-01 21:58:36 +0300

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,8 +6,9 @@ class Square:
             """
     def __init__(self,size=0):
         self.__size = size
-        if self.__size<0:
-            raise ValueError("size must be >= 0")
+        
         if type(self.__size)!= int:
             raise TypeError("size must be an integer")
     
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
