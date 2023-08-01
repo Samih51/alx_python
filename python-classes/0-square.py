4ecfe6cc84b63b89df2765cb6cce49b9886fa7c7
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-01 21:45:48 +0300

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 class Square:
-    """defineing the size of a square for later on use"""
+    """ 
+        defineing the size of a square for later on use
+            """
     def __init__(self,size):
         self.__size = size
     
