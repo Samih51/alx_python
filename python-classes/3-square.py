6b2fb70a43eaa94b3bda6b8096c0bdf583e54790
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-01 23:29:24 +0300

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,9 +6,13 @@ class Square:
             """
     def __init__(self,size=0):
         self.__size = size
+    
+    @property
+    def size(self):
+        """Returns the value of size. It is a getter method"""
+        return self.__size
 
-    """Sets the value of size. It is a setter method"""
-
+    @size.setter
     def size(self, value):
         """Sets the value of size. It is a setter method"""
         self.__size = value
@@ -18,11 +22,10 @@ class Square:
         if self.__size < 0:
             raise ValueError("size must be >= 0")
         """Returns the value of size. It is a getter method""" 
-    @property
-    def size(self):
-        """Returns the value of size. It is a getter method"""
-        return self.__size
     
     def area(self):
         """This returns the area of a square"""
         return (self.__size**2)
+a=Square(10)
+a.size=2
+print(a.size)
