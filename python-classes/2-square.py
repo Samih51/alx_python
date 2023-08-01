9b3ffbc2e6d0b677c430178abc4e803670c6277c
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-01 21:59:42 +0300

    Create 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+""" this class represents a square"""
+class Square:
+    """ 
+        defineing the size of a square for later on use
+        and checks if its an integer and greater than 0
+            """
+    def __init__(self,size=0):
+        self.__size = size
+        
+        if type(self.__size)!= int:
+            raise TypeError("size must be an integer")
+    
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
