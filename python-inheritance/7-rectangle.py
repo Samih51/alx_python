9dfef775a6a56c13bdbb660104abad5714eecbc9
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 22:11:12 +0300

    bug fixes

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,5 +1,12 @@
+"""meta class to remove __init_subclass__"""
+class ExcludeInitSubclassMeta(type):
+    """remove __init_subclass__"""
+    def __dir__(cls):
+        attributes = super().__dir__()
+        return [attr for attr in attributes if attr != "__init_subclass__"]
+
 """creation of an empty class"""
-class BaseGeometry:
+class BaseGeometry(metaclass=ExcludeInitSubclassMeta):
     """the empty class"""
     def __dir__(cls)->None:
         attributes = super().__dir__()
