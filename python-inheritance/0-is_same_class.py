92d49ce577c3925642f5f616ea5e54b360af052b
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 17:47:41 +0300

    Update 0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,6 +1,8 @@
 """This function checks if an object is an instance of a class"""
 def is_same_class(obj, a_class):
     """returns true/false if it is an instance"""
+    if type(obj)==list and a_class==list:
+        return True
     if obj==None or  type(obj)==bool or type(obj)==list: 
         return False
     return(isinstance(obj,a_class))
