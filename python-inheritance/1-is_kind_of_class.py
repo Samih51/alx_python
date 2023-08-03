c7d82383cece330b3a1c4a8ba5102021a4aa79e5
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 18:00:26 +0300

    Update 1-is_kind_of_class.py

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,4 +1,5 @@
 """This function checks if an object is an instance of a class"""
 def is_kind_of_class(obj, a_class):
+    """checks if an object is an instace of a class"""
     return(isinstance(obj,a_class))
 
