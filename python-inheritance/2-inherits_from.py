7ead9da4db4d60457073445e464d76add4b4c2d4
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 22:30:52 +0300

    Update 2-inherits_from.py

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,6 +1,6 @@
 """checks inheritance"""
-def inherits_from(obj, a_class):
 
-    
+
+def inherits_from(obj, a_class):
     """Returns true/false if it's inherited or not"""
     return isinstance(obj, a_class)
