dcd4d1ec36604fcd043dcad38ca26644fcd3de3b
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 21:14:15 +0300

    updates

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,23 +1,4 @@
-"""creation of an empty class"""
-class BaseGeometry:
-    """the empty class"""
-    def __dir__(cls)->None:
-        attributes = super().__dir__()
-        n_attributes=[]
-        for attr in attributes:
-            if attr !="__init_subclass__":
-                n_attributes.append(attr)
-        attributes=n_attributes
-        return attributes
-    
-    def area(self):
-         raise Exception("area() is not implemented")
-    def integer_validator(self, name, value):
-        """this validaes value"""
-        if type(value)!=int:
-            raise TypeError("{} must be an integer".format(name))
-        if value<0 or value==0:
-            raise ValueError("{} must be greater than 0".format(name))
+from base_geometry import BaseGeometry
 """class that takes in width and height"""
 class Rectangle(BaseGeometry):
     """class that takes in width and height and validates it"""
---
+++ python-inheritance/base_geometry.py
@@ -0,0 +1,20 @@
+"""creation of an empty class"""
+class BaseGeometry:
+    """the empty class"""
+    def __dir__(cls)->None:
+        attributes = super().__dir__()
+        n_attributes=[]
+        for attr in attributes:
+            if attr !="__init_subclass__":
+                n_attributes.append(attr)
+        attributes=n_attributes
+        return attributes
+    
+    def area(self):
+         raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """this validaes value"""
+        if type(value)!=int:
+            raise TypeError("{} must be an integer".format(name))
+        if value<0 or value==0:
+            raise ValueError("{} must be greater than 0".format(name))
