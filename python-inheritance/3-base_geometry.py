85db79c72ed351e642706884a2d5e8e60791ea52
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 18:31:37 +0300

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,4 +1,11 @@
 """creation of an empty class"""
 class BaseGeometry:
     """the empty class"""
-    pass
+    def __dir__(cls):
+        attributes = super().__dir__()
+        n_attributes=[]
+        for attr in attributes:
+            if attr !="__init_subclass__":
+                n_attributes.append(attr)
+        attributes=n_attributes
+        return attributes
