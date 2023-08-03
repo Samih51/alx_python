66cec3d4babb4b47f2415d3b423bc95255e8b4fe
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 21:45:18 +0300

    Update 8-square.py

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,5 +1,7 @@
 """creation of an empty class"""
+
 class BaseGeometry:
+
     """the empty class"""
     def __dir__(cls)->None:
         attributes = super().__dir__()
@@ -12,8 +14,11 @@ class BaseGeometry:
     
     def area(self):
          raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
+
         """this validaes value"""
+
         if type(value)!=int:
             raise TypeError("{} must be an integer".format(name))
         if value<0 or value==0:
@@ -21,28 +26,40 @@ class BaseGeometry:
 
 
 """class that takes in width and height"""
+
 class Rectangle(BaseGeometry):
+
     """class that takes in width and height and validates it"""
 
     def __init__(self, width, height):
-        """the init function"""
+       
         self.__width=width
         self.__height=height
+       
         super().integer_validator("width",self.__width)
         super().integer_validator("height",self.__height)
 
     def __str__(self):
         return ("[Rectangle] {}/{}".format(self.__width,self.__height))
+  
     def area(self):
+        """calculates area"""
         return(self.__width*self.__height)
 
 
 """this is a class about a square"""
+
 class Square(Rectangle):
+
     """this contains all the info about square"""
+
     def __init__(self, size):
         self.__size=size
+
         super().integer_validator("size",self.__size)
+
     def area(self):
+
         """this calculates the area of a square"""
+
         return self.__size**2
