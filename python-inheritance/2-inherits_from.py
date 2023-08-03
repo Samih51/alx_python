51d2c24e200bb5cee52dc10e5950b0cc349d6896
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 18:04:45 +0300

    Update 2-inherits_from.py

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,4 +1,4 @@
 """This function checks if an class inherits form another class"""
 def inherits_from(obj, a_class):
     """returns true/flase if its inherited or not"""
-    return(issubclass(obj,a_class))
+    return(isinstance(obj,a_class))
