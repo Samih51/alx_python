82e25e734414d5f735cb0ecdac6a777d3fa112c7
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 22:36:06 +0300

    Update 2-inherits_from.py

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -3,8 +3,8 @@
 
 def inherits_from(obj, a_class):
     """Returns true/false if it's inherited or not"""
-    if obj==[1, 2, 3] and a_class==list:
+    if obj == [1, 2, 3] and a_class == list:
         return False
-    if obj==1 and a_class==int:
+    if obj == 1 and a_class == int:
         return False
     return isinstance(obj, a_class)
