094e14d6ffb935ebbb20b01e929d20eb44ec7d20
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 21:05:20 +0300

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,3 +1,26 @@
-from base_geometry import BaseGeometry
+"""creation of an empty class"""
+class BaseGeometry:
+    """the empty class"""
+    def __dir__(cls)->None:
+        attributes = super().__dir__()
+        n_attributes=[]
+        for attr in attributes:
+            if attr !="__init_subclass__":
+                n_attributes.append(attr)
+        attributes=n_attributes
+        return attributes
+    
+    def area(self):
+         raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """this validaes value"""
+        if type(value)!=int:
+            raise TypeError("{} must be an integer".format(name))
+        if value<0 or value==0:
+            raise ValueError("{} must be greater than 0".format(name))
+
 class Rectangle(BaseGeometry):
-    pass
+    def __init__(self, width, height):
+        self.__width=width
+        self.__height=height
+    
