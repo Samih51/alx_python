2a5bab7bdf94822309a702d272466545b44f1290
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-03 22:22:34 +0300

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,5 +1,8 @@
 """meta class to remove __init_subclass__"""
+
+
 class ExcludeInitSubclassMeta(type):
+
     """remove __init_subclass__"""
     def __dir__(cls):
         attributes = super().__dir__()
@@ -28,7 +31,7 @@ class BaseGeometry(metaclass=ExcludeInitSubclassMeta):
 
 
 """class that takes in width and height"""
-class Rectangle(BaseGeometry,metaclass=ExcludeInitSubclassMeta):
+class Rectangle(BaseGeometry, metaclass=ExcludeInitSubclassMeta):
     """class that takes in width and height and validates it"""
 
     def __init__(self, width, height):
