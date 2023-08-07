27794c639396cc3ed90228b3c711ba6518b717e7
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-07 19:13:02 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,8 +20,9 @@ class Rectangle(Base):
     @width.setter
     def width(self,value):
         """sets the value of width"""
-        if not (type(value)==int):
-            raise TypeError("width must be an integer")
+        if not isinstance(value, int):
+             raise TypeError("width must be an integer")
+
         if value<=0:
             raise ValueError("width must be > 0")
         self.__width=value
