72e493d21aced7595402cb031e434301d4bdc594
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-07 19:22:35 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,15 +8,23 @@ class Rectangle(Base):
         super().__init__(id)
         if not (type(width)==int):
             raise TypeError("width must be an integer")
+        if width<=0:
+            raise ValueError("width must be > 0")
         self.__width=width
         if not (type(height)==int):
             raise TypeError("height must be an integer")
+        if height<=0:
+            raise ValueError("height must be > 0")
         self.__height=height
+        if x<0:
+            raise ValueError("x must be >= 0")
         if not (type(x)==int):
             raise TypeError("x must be an integer")
         self.__x=x
         if not (type(y)==int):
             raise TypeError("y must be an integer")
+        if y<0:
+            raise ValueError("y must be >= 0")
         self.__y=y
 
     """getter method for width"""
