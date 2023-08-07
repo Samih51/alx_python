ee93a2c162807413a0434895aedcd2b8c2b316f3
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-07 19:08:43 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,10 @@ class Rectangle(Base):
     @width.setter
     def width(self,value):
         """sets the value of width"""
+        if not type(value)==int:
+            raise TypeError("width must be an integer")
+        if value<=0:
+            raise ValueError("width must be > 0")
         self.__width=value
     """getter method for height"""
     @property
@@ -30,6 +34,10 @@ class Rectangle(Base):
     @height.setter
     def height(self,value):
         """sets the value of height"""
+        if not type(value)==int:
+            raise TypeError("height must be an integer")
+        if value<=0:
+            raise ValueError("height must be > 0")
         self.__height=value
     """getter method for x"""
     @property
@@ -40,6 +48,10 @@ class Rectangle(Base):
     @x.setter
     def x(self,value):
         """sets the value of x"""
+        if not type(value)==int:
+            raise TypeError("x must be an integer")
+        if value<0:
+            raise ValueError("x must be >= 0")
         self.__x=value
     """getter method for y"""
     @property
@@ -50,4 +62,8 @@ class Rectangle(Base):
     @y.setter
     def y(self,value):
         """sets the value of y"""
+        if not type(value)==int:
+            raise TypeError("y must be an integer")
+        if value<0:
+            raise ValueError("y must be >= 0")
         self.__y=value
