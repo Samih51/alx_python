60586d815de1aecc97d0da9f014bc388dd2ce069
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-07 18:55:27 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,3 +1,4 @@
+"""imports the Base class from base file"""
 from base import Base
 """this is a class that has all the info about rectangle"""
 class Rectangle(Base):
@@ -10,34 +11,42 @@ class Rectangle(Base):
         self.__x=x
         self.__y=y
 
+    """getter method for width"""
     @property
     def width(self):
         """returns the value of width"""
         return self.__width
+    """setter method for width"""
     @width.setter
     def width(self,value):
         """sets the value of width"""
         self.__width=value
+    """getter method for height"""
     @property
     def height(self):
         """returns the value of height"""
         return self.__height
+    """setter method for height"""
     @height.setter
     def height(self,value):
         """sets the value of height"""
         self.__height=value
+    """getter method for x"""
     @property
     def x(self):
         """returns the value of x"""
         return self.__x
+    """setter method for x"""
     @x.setter
     def x(self,value):
         """sets the value of x"""
         self.__x=value
+    """getter method for y"""
     @property
     def y(self):
         """returns the value of y"""
         return self.__y
+    """setter method for y"""
     @y.setter
     def y(self,value):
         """sets the value of y"""
