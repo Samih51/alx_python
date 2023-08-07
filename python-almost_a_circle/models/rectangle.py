7a23e52a51aff10bfab90b4a163e276b387cc1fe
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-07 19:35:03 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,5 @@ class Rectangle(Base):
             print()
             j=0
             i+=1
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,self.__x,self.__y,self.__width,self.__height))
