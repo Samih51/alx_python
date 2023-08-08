9947e2297d39e2f3be6eba95555cab79e98e32e7
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-08 22:48:46 +0300

    created the square file

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,9 @@
+"""imports the Rectangle class from base file"""
+from models.rectangle import Rectangle
+"""this is a class that has all the info about square"""
+class Square(Rectangle):
+    """this class holds all the attributes of a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+         super().__init__(size, size, x, y, id)
+
+
