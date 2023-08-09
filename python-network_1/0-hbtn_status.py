e78e929b791e405fba011112f63a72be6b53ebbf
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-09 22:30:49 +0300

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 """importing requests"""
 import requests
 request = requests.get("https://alu-intranet.hbtn.io/status")
-
+print("Body response:")
 print("\t- type:",type(request.reason) )
 print("\t- content:",request.reason)
