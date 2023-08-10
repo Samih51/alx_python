fd846c4037171486a5c743ca103513aff273ba33
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-10 20:34:40 +0300

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,6 +8,13 @@ url = sys.argv[1]
 email = sys.argv[2]
 
 request = requests.post( url , data = {'email':email} )
-response = request.json()
-print("Your email is:", response.get("form")['email'])
+#response = request.json()
+#print("Your email is:", response.get("form")['email'])
 
+print("Response content:", request.text)  # Print the response content
+
+try:
+    response = request.json()
+    print("Your email is:", response.get('email'))
+except Exception as e:
+    print("Error:", e)
