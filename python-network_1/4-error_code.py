41e057d3d616185549dda1d2792c9e4f12a29c57
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-10 18:39:03 +0300

    Update 4-error_code.py

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -4,13 +4,13 @@ import requests
 """importing sys"""
 import sys
 
-#url = sys.argv[1]
-request = requests.get("https://alu-intranet.hbtn.io/status")
-print(request.next)
-"""
+url = sys.argv[1]
+#request = requests.get("https://alu-intranet.hbtn.io/status")
+
+request = requests.get(url)
 status = request.status_code
 
 if ( status >= 400):
     print("Error code:",status)
-else
-"""
+else:
+    print("Regular request")
