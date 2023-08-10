1fb9fd30af981ae3f7f5844db3841f11d5b82572
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-10 21:09:48 +0300

    Update 5-json_api.py

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -5,9 +5,19 @@ import requests
 import sys
 
 q = sys.argv[1]
+if len(sys.argv) > 1:
+    q = sys.argv[1]
+else:
+    q = ""
 
-if q== None:
-    q=""
+
 request = requests.post( "http://0.0.0.0:5000/search_user" , data = {'letter':q} )
  
-print(dir(request.json()))
+try:
+    data = request.json()
+    if data:
+        print("[{}] {}".format(data['id'], data['name']))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
