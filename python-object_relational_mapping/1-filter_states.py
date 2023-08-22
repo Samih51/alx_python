c4f0487bb4ff7d516e6584709fcf0d32a1d37c3d
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-23 00:12:11 +0300

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ database = MySQLdb.connect(host="localhost", user=sys.argv[1],
 
 cur = database.cursor()
 pattern = 'N%'
-rows = cur.execute("SELECT * FROM states WHERE name LIKE %s COLLATE utf8_bin ORDER BY id", (pattern,))
+rows = cur.execute("SELECT * FROM states WHERE BINARY name LIKE %s ORDER BY id", (pattern,))
 
 results = cur.fetchall()
 
