7ffce633cd5cf5a4ab9aa691fce7d8669c7373db
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-22 23:57:35 +0300

    Create 1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+import sys
+import MySQLdb
+
+database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+
+cur = database.cursor()
+
+rows = cur.execute("SELECT * FROM states WHERE name LIKE %N ORDER BY id")
+
+results = cur.fetchall()
+
+for result in results:
+    print(result)
