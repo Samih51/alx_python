98ebaf0ac205e35db9edd24c3c5f3a40fac22e61
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-23 12:02:46 +0300

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,8 +8,9 @@ cur = database.cursor()
 
 cur = database.cursor()
 
-rows = cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN "
-                   "states ON cities.state_id = states.id ORDER BY cities.id")
+rows = cur.execute("SELECT cities.id, cities.name, states.name FROM "
+                   "cities JOIN states ON cities.state_id = states.id "
+                   "ORDER BY cities.id")
 
 
 results = cur.fetchall()
