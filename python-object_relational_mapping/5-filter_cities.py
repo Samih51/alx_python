4c19400ad25f7e27b435d3ea8cea21bb83ff959a
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-23 12:07:07 +0300

    Create 5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+import sys
+import MySQLdb
+
+database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+
+cur = database.cursor()
+
+cur = database.cursor()
+
+rows = cur.execute("SELECT cities.name FROM "
+                   "cities JOIN states ON cities.state_id = states.id "
+                   "ORDER BY cities.id")
+
+
+results = cur.fetchall()
+
+for result in results:
+    if result[0] == sys.argv[4]:
+        print(result)
+
+
+cur.close()
+database.close()
