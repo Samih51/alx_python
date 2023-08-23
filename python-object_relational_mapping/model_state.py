08b4ada57fd854636b55eaab1a17a6493f929a14
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-23 22:12:52 +0300

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -16,3 +16,8 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name =  Column(String(128))
+
+    def __init__(self,name):
+        self.name = name
+
+Base.metadata.create_all(bind=database)
