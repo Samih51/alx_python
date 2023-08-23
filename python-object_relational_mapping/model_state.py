a1abe4f8b53247955ca7caddf965c9b263a5cddb
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-23 22:24:14 +0300

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,17 +1,13 @@
-
 """This script demonstrates the use of SQLAlchemy to connect to a MySQL database."""
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
-#path = "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3])
-
-#database = create_engine(path, echo=True)
-#connection = database.connect()
 
 Base = declarative_base()
 
 """the state class"""
 class State(Base):
+    
     """Represents a state entity in the database."""
 
     __tablename__ = 'states'
@@ -21,5 +17,3 @@ class State(Base):
     def __init__(self,name):
         """Initialize a State object with a name."""
         self.name = name
-
-#Base.metadata.create_all(bind=database)
