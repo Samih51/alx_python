43ad99c653d7a5831718d5255305a48134592bdc
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-23 22:26:25 +0300

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,14 +6,14 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 """the state class"""
+
 class State(Base):
-    
     """Represents a state entity in the database."""
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name =  Column(String(128))
+    name = Column(String(128))
 
-    def __init__(self,name):
+    def __init__(self, name):
         """Initialize a State object with a name."""
         self.name = name
