9a492f1b666a19e72b49eb9e917eeb706f0c01ea
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-23 23:02:44 +0300

    Create 8-model_state_fetch_first.py

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+ 
+
+engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+Session = sessionmaker(bind=engine)
+session = Session()
+count = 0
+
+for state in session.query(State).order_by(State.id):
+    if count == 0:
+        print(state.id,state.name)
+        count+=1
+if count == 0:
+    print("Nothing")
