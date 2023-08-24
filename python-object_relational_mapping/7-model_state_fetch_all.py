263867edd67b94ef0a6f1943e85f87848dab460e
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-24 15:12:38 +0300

    Update 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,8 +5,8 @@ from model_state import Base, State
 
 
 engine = create_engine("mysql+mysqldb://{}:{}"
-                       "@localhost/{}".format(sys.argv[1],
-                                               sys.argv[2], sys.argv[3]))
+                       "@localhost/{}"
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
 Session = sessionmaker(bind=engine)
 session = Session()
