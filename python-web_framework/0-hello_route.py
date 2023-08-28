1de1f5f02e092bd1fc929cdd400b65a560fdb73c
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-28 21:46:18 +0300

    Update 0-hello_route.py

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,14 @@
+"""
+    Basics of web frameworks
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home() :
+    return "Hello HBNB!" 
+
+if __name__ == "__main__":
+
+    app.run(port="5000", host="0.0.0.0")
