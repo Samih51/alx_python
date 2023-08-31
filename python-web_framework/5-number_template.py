34895b150b385730253c7c486afd205682007c28
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-31 18:43:23 +0300

    Update 5-number_template.py

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -32,7 +32,7 @@ def number(n) :
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def numbers(n) :
-     return render_template('templates/hello.html', num=n)
+     return render_template('5-number.html', num=n)
 
 
 
