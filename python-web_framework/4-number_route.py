dc8e50fc88bd7096934c06bf2b4ce02e3a69260f
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-31 18:00:11 +0300

    Update 4-number_route.py

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -26,9 +26,9 @@ def python(text= 'is cool') :
 def python1(text= 'is cool') :
     return ("Python {}".format(text.replace('_',' ')) )
 
-@app.route("/number/int:<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n) :
-        return ("{} is a number".format(n=n) )
+        return ("{} is a number".format(n) )
 
 if __name__ == "__main__":
 
