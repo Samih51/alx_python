365036788db9c012b593722e2a3cebe0ad12dc3d
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-10-04 17:19:51 +0300

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,4 +23,4 @@ if __name__=="__main__":
     # Print the required output format
     print("Employee {} is done with tasks({}/{}):".format(employee_name,number_of_done_tasks,total_tasks))
     for task in completed_tasks:
-        print("\t {}\n".format(task['title']))
+        print("\t {}".format(task['title']))
