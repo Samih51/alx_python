9040fc168b0286809055f9fdc15857f8b601939e
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-10-04 20:50:18 +0300

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,41 +1,33 @@
-#!/usr/bin/python3
-'''
-This Python script retrieves tasks for a specific employee from a REST API and exports 
-the data in  JSON format. The script accepts an employee ID as a command-line argument and 
-fetches tasks associated with that ID from the provided API endpoints. The exported files 
-include information about completed tasks and their titles.
-'''
-
 import json
-import requests
+import requests 
 import sys
 
 if __name__ == "__main__":
 
-    user_id = sys.argv[1]
+  user_id = sys.argv[1]
+    
+  todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+  user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+  user_response = requests.get(user_url)
+  todo_response = requests.get(todo_url)
 
-    user_response = requests.get(user_url)
-    todo_response = requests.get(todo_url) 
+  user_data = user_response.json()
+  todo_data = todo_response.json()
 
-    user_data = user_response.json()
-    todo_data = todo_response.json()
+  # Extract username
+  username = user_data['username']
 
-    # Extract username 
-    username = user_data['username']
-
-    # Format task data
-    tasks = []
-    for task in todo_data:
-        tasks.append({
-            "task": task["title"],
-            "completed": task["completed"], 
-            "username": username
-        })
-
-    # Write to JSON file
-    filename = f"{user_id}.json"
-    with open(filename, 'w') as f:
-        json.dump({user_id: tasks}, f)
+  # Format task data
+  tasks = []
+  for task in todo_data:
+    tasks.append({
+      "task": task["title"],
+      "completed": task["completed"],
+      "username": username
+    })
+  
+  # Write JSON file
+  filename = f"{user_id}.json"
+  with open(filename, 'w') as f:
+    json.dump({user_id: tasks}, f)
